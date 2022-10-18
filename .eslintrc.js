9932c2179e4783a4e78fde384495686ae93951b8
Jamie McNeil <jamievmcneil@gmail.com>
2022-10-18 18:02:23 -0400

    Add and configure ESLint in project

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+	'env': {
+		'node': true,
+		'es2021': true,
+		'jest': true,
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+	],
+	'overrides': [
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module',
+		'project': './tsconfig.json',
+	},
+	'plugins': [
+		'react',
+		'react-hooks',
+		'@typescript-eslint'
+	],
+	'settings': {
+		'react': {
+			'version': 'detect'
+		}
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		],
+		'react/jsx-uses-react': 'off',
+		'react/react-in-jsx-scope': 'off'
+	}
+};
