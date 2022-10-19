70f2208cc53a74e98f957785433a825625194270
Jamie McNeil <jamievmcneil@gmail.com>
2022-10-19 08:26:39 -0400

    Re-add StatusBar and add todo

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar';
 import { Button } from '@rneui/base';
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <View style={styles.container}>
+        {/* TODO: Change status bar color based on theme */}
+        <StatusBar style="dark" />
         <Text>Hello there!</Text>
         <Button title="Hello World" />
       </View>
