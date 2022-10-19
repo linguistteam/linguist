d140004b38ebde9904c189aa34fb025a2cd447f3
Jamie McNeil <jamievmcneil@gmail.com>
2022-10-19 08:22:59 -0400

    Remove MUI; Add React Native Elements and get it working.

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from '@rneui/base';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello there!</Text>
-      <Text>Test!</Text>
-      <StatusBar style="auto" />
+      <Button title="Hello World" />
     </View>
   );
 }
