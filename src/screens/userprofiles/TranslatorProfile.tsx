45897788b67926bd7876ceef69bba7d5b9d778ec
Jamie McNeil <jamievmcneil@gmail.com>
2022-10-20 17:54:16 -0400

    Add safe inset area to profile View

--- src/screens/userprofiles/TranslatorProfile.tsx
+++ src/screens/userprofiles/TranslatorProfile.tsx
@@ -1,16 +1,20 @@
 import { Image, View } from 'react-native';
 import { Text } from '@rneui/base';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { styles } from './styles';
 
 // TODO: Fix eslint indent issues
 const TranslatorProfile = () => {
+    const insets = useSafeAreaInsets();
+
     // TODO: Image should be uploaded by user and pass in from DB
     const image = { uri: 'https://via.placeholder.com/400x400' };
 
     return (
-        <View style={styles.container}>
-            <Image source={image} resizeMode="cover" style={styles.profileImage} />
-            {/* <Text h1>Hello</Text> */}
+        <View style={{ paddingTop: insets.top }}>
+            <View style={styles.container}>
+                <Image source={image} style={styles.profileImage} />
+            </View>
         </View>
     );
 };
