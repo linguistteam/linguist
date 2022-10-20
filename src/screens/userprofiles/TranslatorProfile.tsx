ca9fa3ad0f57dd7002f590620ec4b0ab855992bf
Jamie McNeil <jamievmcneil@gmail.com>
2022-10-20 07:33:28 -0400

    Set up initial TranslatorProfile; export from index

---
+++ src/screens/userprofiles/TranslatorProfile.tsx
@@ -0,0 +1,13 @@
+import { View } from 'react-native';
+import { Text } from '@rneui/base';
+
+// TODO: Fix eslint indent issues
+const TranslatorProfile = () => {
+    return (
+        <View>
+            <Text h1>Hello</Text>
+        </View>
+    );
+};
+
+export default TranslatorProfile;
---
+++ src/screens/userprofiles/index.ts
@@ -0,0 +1,1 @@
+export { default as TranslatorProfile } from './TranslatorProfile';
