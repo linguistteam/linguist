6a72d003b5c7cbe1819580f015337bfba7397e72
Jamie McNeil <jamievmcneil@gmail.com>
2022-10-22 15:10:47 -0400

    Remove commented formatting code

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,38 +34,3 @@ module.exports = {
     project: './tsconfig.json',
   },
 };
-// env: {
-//   node: true,
-//   es2021: true,
-//   jest: true,
-// },
-// extends: [
-//   'eslint:recommended',
-//   'plugin:react/recommended',
-//   'plugin:react-hooks/recommended',
-//   'plugin:@typescript-eslint/recommended',
-//   'plugin:@typescript-eslint/eslint-recommended',
-//   'plugin:@typescript-eslint/recommended-requiring-type-checking',
-// ],
-// overrides: [],
-// parser: '@typescript-eslint/parser',
-// parserOptions: {
-//   ecmaVersion: 'latest',
-//   sourceType: 'module',
-//   project: './tsconfig.json',
-// },
-// plugins: ['prettier', 'react', 'react-hooks', '@typescript-eslint'],
-// settings: {
-//   react: {
-//     version: 'detect',
-//   },
-// },
-// rules: {
-//   indent: ['error', 2, { SwitchCase: 1, ignoredNodes: ['ConditionalExpression'] }],
-//   quotes: ['error', 'single', { avoidEscape: true }],
-//   semi: ['error', 'always'],
-//   'prettier/prettier': 'error',
-//   'react/jsx-uses-react': 'off',
-//   'react/react-in-jsx-scope': 'off',
-// },
-// };
