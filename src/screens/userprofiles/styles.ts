b6a99b87360ec9614fd330b58889a3f98faedf70
Jamie McNeil <jamievmcneil@gmail.com>
2022-10-22 09:57:52 -0400

    Add content padding

--- src/screens/userprofiles/styles.ts
+++ src/screens/userprofiles/styles.ts
@@ -11,5 +11,6 @@ export const styles = StyleSheet.create({
         borderTopLeftRadius: 20,
         borderTopRightRadius: 20,
         backgroundColor: '#ffffff',
+        padding: 20,
     },
 });
