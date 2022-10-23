21f26c3b577bff00c2ddd1929697730c536bb30a
Jamie McNeil <jamievmcneil@gmail.com>
2022-10-23 18:33:09 -0400

    Add initial StarRatingDisplay and styles

--- src/components/userprofiles/UserRating.tsx
+++ src/components/userprofiles/UserRating.tsx
@@ -1,10 +1,17 @@
 import { View } from 'react-native';
 import { Text } from 'native-base';
+import { StarRatingDisplay } from 'react-native-star-rating-widget';
+import { styles } from './styles';
 
 const UserRating = () => {
+  const rating = 4.5;
+
   return (
-    <View>
-      <Text>Rating goes here</Text>
+    <View style={styles.container}>
+      <Text bold color="#2980F2">
+        {rating}
+      </Text>
+      <StarRatingDisplay color="#2980F2" rating={rating} />
     </View>
   );
 };
