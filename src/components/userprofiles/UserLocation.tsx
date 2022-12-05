bcd30c7ef947e3940f61efc5990abdb197829c4c
Jamie McNeil <jamievmcneil@gmail.com>
2022-12-05 08:13:44 -0500

    Add initial UserLocation component.

---
+++ src/components/userprofiles/UserLocation.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'native-base';
+
+const UserLocation = () => {
+  const userLocation = 'Philadeplhia, PA';
+
+  return (
+    <View>
+      <Text>{userLocation}</Text>
+    </View>
+  );
+};
+
+export default UserLocation;
