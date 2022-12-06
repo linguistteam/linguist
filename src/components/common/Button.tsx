a5d80cf4ca0906dc5744161c2e952f7c5958bc68
Jamie McNeil <jamievmcneil@gmail.com>
2022-12-06 10:56:43 -0500

    Add onPress and width to Button

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,12 +1,21 @@
+import { GestureResponderEvent } from 'react-native';
 import { Button as NativeBaseButton, Text } from 'native-base';
 import Colors from '@assets/colors';
 
 type ButtonProps = {
+  onPress: (event: GestureResponderEvent) => void;
   text: string;
+  width?: number | string;
 };
 
-const Button = ({ text }: ButtonProps) => (
-  <NativeBaseButton bgColor={Colors.blueMagenta} borderRadius={20} height={45}>
+const Button = ({ onPress, text, width }: ButtonProps) => (
+  <NativeBaseButton
+    onPress={onPress}
+    bgColor={Colors.blueMagenta}
+    borderRadius={20}
+    height={45}
+    width={width}
+  >
     <Text color={Colors.white} fontSize={15} fontWeight={700} textTransform="uppercase">
       {text}
     </Text>
