c983ee426ca8aa821665e5c1bb96c9481a683eff
Jamie McNeil <jamievmcneil@gmail.com>
2022-12-06 11:27:07 -0500

    Lighten Button bg color when pressed

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { GestureResponderEvent } from 'react-native';
 import { Button as NativeBaseButton, Text } from 'native-base';
 import Colors from '@assets/colors';
@@ -8,18 +9,24 @@ type ButtonProps = {
   width?: number | string;
 };
 
-const Button = ({ onPress, text, width }: ButtonProps) => (
-  <NativeBaseButton
-    onPress={onPress}
-    bgColor={Colors.blueMagenta}
-    borderRadius={20}
-    height={45}
-    width={width}
-  >
-    <Text color={Colors.white} fontSize={15} fontWeight={700} textTransform="uppercase">
-      {text}
-    </Text>
-  </NativeBaseButton>
-);
+const Button = ({ onPress, text, width }: ButtonProps) => {
+  const [isPressed, setIsPressed] = useState(false);
+
+  return (
+    <NativeBaseButton
+      onPress={onPress}
+      bgColor={isPressed ? Colors.lightenedBlueMagenta : Colors.blueMagenta}
+      borderRadius={20}
+      height={45}
+      width={width}
+      onPressIn={() => setIsPressed(true)}
+      onPressOut={() => setIsPressed(false)}
+    >
+      <Text color={Colors.white} fontSize={15} fontWeight={700} textTransform="uppercase">
+        {text}
+      </Text>
+    </NativeBaseButton>
+  );
+};
 
 export default Button;
