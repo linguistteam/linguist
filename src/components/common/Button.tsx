fe07c55814213526ccd560c52e53f0e31ed032dc
Jamie McNeil <jamievmcneil@gmail.com>
2022-12-06 09:53:35 -0500

    Style Button

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,9 +1,16 @@
-import { Button as NativeBaseButton } from 'native-base';
+import { Button as NativeBaseButton, Text } from 'native-base';
+import Colors from '../../assets/colors';
 
 type ButtonProps = {
   text: string;
 };
 
-const Button = ({ text }: ButtonProps) => <NativeBaseButton>{text}</NativeBaseButton>;
+const Button = ({ text }: ButtonProps) => (
+  <NativeBaseButton bgColor={Colors.blueMagenta} borderRadius={20} height={45}>
+    <Text color={Colors.white} fontSize={15} fontWeight={700} textTransform="uppercase">
+      {text}
+    </Text>
+  </NativeBaseButton>
+);
 
 export default Button;
