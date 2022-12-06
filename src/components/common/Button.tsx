f8b239f10dcf97403c7cd0bc3c129de23184702c
Jamie McNeil <jamievmcneil@gmail.com>
2022-12-06 09:42:18 -0500

    Simplify Button

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,15 +1,9 @@
-import { Button as NativeBaseButton, View } from 'native-base';
+import { Button as NativeBaseButton } from 'native-base';
 
 type ButtonProps = {
   text: string;
 };
 
-const Button = ({ text }: ButtonProps) => {
-  return (
-    <View>
-      <NativeBaseButton>{text}</NativeBaseButton>
-    </View>
-  );
-};
+const Button = ({ text }: ButtonProps) => <NativeBaseButton>{text}</NativeBaseButton>;
 
 export default Button;
