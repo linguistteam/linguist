3785a091f5ab74d8784720e0e1e6aed5a3e48c4d
Jamie McNeil <jamievmcneil@gmail.com>
2022-12-08 08:32:05 -0500

    Import and use languageMatch

--- src/components/userprofiles/Languages.tsx
+++ src/components/userprofiles/Languages.tsx
@@ -1,11 +1,13 @@
 import { Flex, Heading, View } from 'native-base';
 import CountryFlag from 'react-native-country-flag';
 import Colors from '@assets/colors';
+import { languageMatch } from '@utils';
 import { languagesStyles } from './styles';
 
 const Languages = () => {
   // TODO: Figure out when we should show England flag vs US flag
   const languages = ['us', 'de', 'jp'];
+  const userLanguages = ['us'];
 
   return (
     <View>
@@ -17,9 +19,14 @@ const Languages = () => {
         {languages.map((language) => (
           <View
             key={language}
-            // TODO: Dynamically set bg color
-            backgroundColor={Colors.transparentBlueMagenta}
-            borderColor={Colors.blueMagenta}
+            backgroundColor={
+              languageMatch(languages, userLanguages, language)
+                ? Colors.transparentBlueMagenta
+                : Colors.transparentGrey
+            }
+            borderColor={
+              languageMatch(languages, userLanguages, language) ? Colors.blueMagenta : Colors.grey
+            }
             style={languagesStyles.countryFlagContainer}
           >
             <CountryFlag isoCode={language} size={25} style={languagesStyles.countryFlag} />
