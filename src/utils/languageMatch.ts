ba2162bbe93fb4c513c0c7b8b680c453c3fb3128
Jamie McNeil <jamievmcneil@gmail.com>
2022-12-08 08:39:52 -0500

    Get languageMatch util not throwing errors

--- src/utils/languageMatch.ts
+++ src/utils/languageMatch.ts
@@ -1,8 +1,5 @@
 /* Takes languages array of two users and returns common language(s) */
-const languageMatch = (
-  languagesArray1: string[],
-  languagesArray2: string[],
-  language: string,
-): boolean => languagesArray1.some(() => languagesArray2.includes(language));
+const languageMatch = (languagesArray1: string[], languagesArray2: string[], language: string) =>
+  languagesArray1.some(() => languagesArray2.includes(language));
 
 export default languageMatch;
