b6dc735d54d49258673a6ebd06aeb5a2e61b0010
Jamie McNeil <jamievmcneil@gmail.com>
2022-12-12 15:37:37 -0500

    Remove utils for now

--- src/utils/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as languageMatch } from './languageMatch';
--- src/utils/languageMatch.ts
+++
@@ -1,5 +0,0 @@
-/* Takes languages array of two users and returns common language(s) */
-const languageMatch = (languagesArray1: string[], languagesArray2: string[], language: string) =>
-  languagesArray1.some(() => languagesArray2.includes(language));
-
-export default languageMatch;
