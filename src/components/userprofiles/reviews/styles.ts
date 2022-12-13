8df534e44b245815570d65dd7221ebea49187d6d
Jamie McNeil <jamievmcneil@gmail.com>
2022-12-13 14:43:17 -0500

    Create new styles in Reviews section

---
+++ src/components/userprofiles/reviews/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export const reviewsStyles = StyleSheet.create({
+    heading: {
+        marginTop: 20,
+        marginBottom: 10,
+    },
+});
+
+export const reviewsSwitcherStyles = StyleSheet.create({
+    reviewsContainer: {
+        marginTop: 10,
+    },
+});
