032e38a5eb2e7113ba9a7ed263d810b9b4fd2046
Jamie McNeil <jamievmcneil@gmail.com>
2022-12-13 15:52:58 -0500

    Add Reviews heading to strings

--- src/assets/strings/en.ts
+++ src/assets/strings/en.ts
@@ -1,5 +1,6 @@
 export const EN = {
     REVIEWS: {
+        HEADING: 'Reviews',
         FROM_CLIENTS: 'From clients',
         FROM_TRANSLATORS: 'From translators',
         NO_REVIEWS_YET: 'No reviews yet.',
