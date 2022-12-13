73fadb0c42f95b80988922f92a5181add10ee79a
Jamie McNeil <jamievmcneil@gmail.com>
2022-12-13 15:53:40 -0500

    Move reviews array to Reviews component, pass down to Switcher

--- src/components/userprofiles/reviews/Reviews.tsx
+++ src/components/userprofiles/reviews/Reviews.tsx
@@ -1,4 +1,5 @@
 import { Heading, View } from 'native-base';
+import { EN } from '@assets/strings';
 import { reviewsStyles } from './styles';
 import Switcher from './Switcher';
 
@@ -6,13 +7,62 @@ interface ReviewsProps {
   isTranslatorProfile: boolean;
 }
 
+export interface ReviewsArrayType {
+  userId: string;
+  profileImage: string;
+  name: string;
+  reviewDate: Date;
+  rating: number;
+  review: string;
+  isTopLinguist?: boolean;
+  isTranslator?: boolean;
+}
+
+const dummyDateToday = new Date();
+const dummyDateYesterday = new Date(Date.now() - 86400000);
+const dummyDateTwoDaysAgo = new Date(Date.now() - 172800000);
+
+// TODO: User id should be generated from react-native-uuid
+const reviews: ReviewsArrayType[] = [
+  {
+    userId: '996c19c0-5aa0-4ad6-b0a2-c33de9034ebc',
+    profileImage: 'Test 1',
+    name: 'Jane Doe',
+    reviewDate: dummyDateToday,
+    rating: 4.5,
+    review: 'Lorem ipsum',
+    isTopLinguist: true,
+    isTranslator: true,
+  },
+  {
+    userId: '3bc88969-df04-44d7-965a-149590e4bf90',
+    profileImage: 'Test 2',
+    name: 'Shane Doe',
+    reviewDate: dummyDateYesterday,
+    rating: 4.8,
+    review: 'Lorem ipsum',
+    isTopLinguist: false,
+    isTranslator: true,
+  },
+  {
+    userId: 'c5ca67d5-a754-465d-add9-7508cfe0d128',
+    profileImage: 'Test 3',
+    name: 'Jane Crow',
+    reviewDate: dummyDateTwoDaysAgo,
+    rating: 5.0,
+    review: 'Lorem ipsum',
+    isTopLinguist: false,
+    isTranslator: false,
+  },
+];
+
 const Reviews = ({ isTranslatorProfile }: ReviewsProps) => (
   <View>
     <Heading bold size="md" style={reviewsStyles.heading}>
-      Reviews
+      {EN.REVIEWS.HEADING}
     </Heading>
 
-    <Switcher isTranslatorProfile={isTranslatorProfile} />
+    <Switcher isTranslatorProfile={isTranslatorProfile} reviews={reviews} />
   </View>
 );
 
