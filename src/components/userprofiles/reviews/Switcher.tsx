4b99e311627eea07368dedb9172480209d49c93d
Jamie McNeil <jamievmcneil@gmail.com>
2022-12-13 15:54:04 -0500

    Grab reviews from props; Add to interface

--- src/components/userprofiles/reviews/Switcher.tsx
+++ src/components/userprofiles/reviews/Switcher.tsx
@@ -3,70 +3,25 @@ import { Flex, Pressable, Text, View } from 'native-base';
 import Colors from '@assets/colors';
 import { EN } from '@assets/strings';
 import { switcherHeaderStyles, switcherStyles } from './styles';
+import { ReviewsArrayType } from './Reviews';
 
 interface SwitcherProps {
   isTranslatorProfile: boolean;
+  reviews: ReviewsArrayType[];
 }
 
-interface ReviewsArrayType {
-  userId: string;
-  profileImage: string;
-  name: string;
-  reviewDate: Date;
-  rating: number;
-  review: string;
-  isTopLinguist?: boolean;
-  isTranslator?: boolean;
-}
-
-const dummyDateToday = new Date();
-const dummyDateYesterday = new Date(Date.now() - 86400000);
-const dummyDateTwoDaysAgo = new Date(Date.now() - 172800000);
-
-// TODO: User id should be generated from react-native-uuid
-const reviews: ReviewsArrayType[] = [
-  {
-    userId: '996c19c0-5aa0-4ad6-b0a2-c33de9034ebc',
-    profileImage: 'Test 1',
-    name: 'Jane Doe',
-    reviewDate: dummyDateToday,
-    rating: 4.5,
-    review: 'Lorem ipsum',
-    isTopLinguist: true,
-    isTranslator: true,
-  },
-  {
-    userId: '3bc88969-df04-44d7-965a-149590e4bf90',
-    profileImage: 'Test 2',
-    name: 'Shane Doe',
-    reviewDate: dummyDateYesterday,
-    rating: 4.8,
-    review: 'Lorem ipsum',
-    isTopLinguist: false,
-    isTranslator: true,
-  },
-  {
-    userId: 'c5ca67d5-a754-465d-add9-7508cfe0d128',
-    profileImage: 'Test 3',
-    name: 'Jane Crow',
-    reviewDate: dummyDateTwoDaysAgo,
-    rating: 5.0,
-    review: 'Lorem ipsum',
-    isTopLinguist: false,
-    isTranslator: false,
-  },
-];
-
-const numberOfReviews = (array: ReviewsArrayType[]) => array.length;
-
-const reviewsFromClients: ReviewsArrayType[] = reviews.filter((review) => !review.isTranslator);
-const reviewsFromTranslators: ReviewsArrayType[] = reviews.filter((review) => review.isTranslator);
-
-const Switcher = ({ isTranslatorProfile }: SwitcherProps) => {
+const Switcher = ({ isTranslatorProfile, reviews }: SwitcherProps) => {
   const [activeHeading, setActiveHeading] = useState({
     clientHeading: true,
     translatorHeading: false,
   });
+
+  const numberOfReviews = (array: ReviewsArrayType[]) => array.length;
+
+  const reviewsFromClients: ReviewsArrayType[] = reviews.filter((review) => !review.isTranslator);
+  const reviewsFromTranslators: ReviewsArrayType[] = reviews.filter(
+    (review) => review.isTranslator,
+  );
 
   return (
     <View>
