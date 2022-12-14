4408e506711d156dd48077cf71863403fb8b0b25
Jamie McNeil <jamievmcneil@gmail.com>
2022-12-14 07:51:58 -0500

    Add lorem ipsum blocks to review text

--- src/components/userprofiles/reviews/Reviews.tsx
+++ src/components/userprofiles/reviews/Reviews.tsx
@@ -32,7 +32,8 @@ const reviews: ReviewType[] = [
     name: 'Jane Crow',
     reviewDate: dummyDateTwoDaysAgo,
     rating: 5.0,
-    review: 'Lorem ipsum',
+    review:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac turpis egestas integer eget aliquet nibh praesent. Dictum sit amet justo donec enim diam vulputate ut. Et ligula ullamcorper malesuada proin libero nunc consequat. Quis ipsum suspendisse ultrices gravida dictum fusce ut placerat orci. Sit amet dictum sit amet justo. Libero id faucibus nisl tincidunt eget nullam. Nibh praesent tristique magna sit amet purus gravida quis. Ultricies integer quis auctor elit sed vulputate mi sit. Suspendisse interdum consectetur libero id faucibus nisl tincidunt. Aliquet eget sit amet tellus cras adipiscing enim. Dictum at tempor commodo ullamcorper a lacus. Sapien faucibus et molestie ac feugiat sed. Vel pretium lectus quam id.',
     isTopLinguist: false,
     isTranslator: false,
   },
@@ -42,7 +43,8 @@ const reviews: ReviewType[] = [
     name: 'Shane Doe',
     reviewDate: dummyDateYesterday,
     rating: 4.8,
-    review: 'Lorem ipsum',
+    review:
+      'Diam donec adipiscing tristique risus nec feugiat in fermentum posuere. Aliquam faucibus purus in massa tempor nec feugiat. Tortor consequat id porta nibh venenatis cras sed. Neque aliquam vestibulum morbi blandit cursus risus at ultrices. Id ornare arcu odio ut sem nulla pharetra diam sit.',
     isTopLinguist: false,
     isTranslator: false,
   },
@@ -52,7 +54,8 @@ const reviews: ReviewType[] = [
     name: 'Jane Moe',
     reviewDate: dummyDateToday,
     rating: 4.7,
-    review: 'Lorem ipsum',
+    review:
+      'Mi sit amet mauris commodo. Dignissim diam quis enim lobortis scelerisque fermentum dui. Arcu risus quis varius quam quisque id diam. Lectus urna duis convallis convallis.',
     isTopLinguist: true,
     isTranslator: true,
   },
@@ -62,7 +65,8 @@ const reviews: ReviewType[] = [
     name: 'Rain Doe',
     reviewDate: dummyDateThreeDaysAgo,
     rating: 4.9,
-    review: 'Lorem ipsum',
+    review:
+      'Etiam dignissim diam quis enim lobortis scelerisque fermentum. Enim blandit volutpat maecenas volutpat blandit. Gravida cum sociis natoque penatibus et magnis dis parturient montes. Sit amet mauris commodo quis imperdiet. Pulvinar elementum integer enim neque volutpat ac. Elementum integer enim neque volutpat. Cursus sit amet dictum sit amet justo donec enim. Cras adipiscing enim eu turpis egestas pretium. Sit amet aliquam id diam maecenas ultricies. Vulputate odio ut enim blandit volutpat maecenas volutpat blandit. Ultrices sagittis orci a scelerisque. Est lorem ipsum dolor sit amet. Ultrices neque ornare aenean euismod elementum nisi quis. Facilisis leo vel fringilla est ullamcorper eget nulla. Interdum velit euismod in pellentesque massa placerat. Sem nulla pharetra diam sit amet nisl suscipit adipiscing.',
     isTopLinguist: false,
     isTranslator: true,
   },
@@ -72,7 +76,7 @@ const reviews: ReviewType[] = [
     name: 'Jane Doe',
     reviewDate: dummyDateTomorrow,
     rating: 4.5,
-    review: 'Lorem ipsum',
+    review: 'Nec sagittis aliquam malesuada bibendum arcu vitae elementum.',
     isTopLinguist: false,
     isTranslator: false,
   },
