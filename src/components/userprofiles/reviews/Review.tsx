ef9ea71ebcd21592ba26b445e235c0d2a057d7b2
Jamie McNeil <jamievmcneil@gmail.com>
2022-12-15 20:34:06 -0500

    Ad read more/read less handling

--- src/components/userprofiles/reviews/Review.tsx
+++ src/components/userprofiles/reviews/Review.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Avatar, Flex, Text, View } from 'native-base';
+import { Avatar, Flex, Pressable, Text, View } from 'native-base';
 import { Moment } from 'moment';
 import { StarRatingDisplay } from 'react-native-star-rating-widget';
 import Colors from '@assets/colors';
@@ -14,17 +14,17 @@ interface ReviewProps {
 }
 
 const Review = ({ name, profileImage, rating, review, reviewDate }: ReviewProps) => {
+  const reviewLongerThan200Chars = review.length > 200;
+
   const [truncateReview, setTruncateReview] = useState(false);
 
   useEffect(() => {
-    if (review && review.length > 200) {
+    if (review && reviewLongerThan200Chars) {
       setTruncateReview(true);
-    } else {
-      setTruncateReview(false);
     }
-  }, [review]);
+  }, [review, reviewLongerThan200Chars]);
 
-  console.log({ review, truncateReview });
+  const truncate = (text: string) => (truncateReview ? `${text.substring(0, 200)}...` : text);
 
   return (
     <View style={reviewStyles.reviewContainer}>
@@ -54,7 +54,16 @@ const Review = ({ name, profileImage, rating, review, reviewDate }: ReviewProps)
             <StarRatingDisplay color={Colors.blueMagenta} rating={rating} starSize={17} />
           </Flex>
 
-          <Text style={reviewStyles.reviewText}>{review}</Text>
+          <Text style={reviewStyles.reviewText}>
+            {truncate(review)}{' '}
+            {reviewLongerThan200Chars && (
+              <Pressable onPress={() => setTruncateReview(!truncateReview)}>
+                <Text bold underline>
+                  {truncateReview ? 'read more' : 'read less'}
+                </Text>
+              </Pressable>
+            )}
+          </Text>
         </View>
       </Flex>
     </View>
