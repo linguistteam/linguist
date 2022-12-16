f4b91241319043b8b50bc43deb9ed74d68324463
Jamie McNeil <jamievmcneil@gmail.com>
2022-12-15 20:05:25 -0500

    Add truncateReview handler

--- src/components/userprofiles/reviews/Review.tsx
+++ src/components/userprofiles/reviews/Review.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { Avatar, Flex, Text, View } from 'native-base';
 import { Moment } from 'moment';
 import { StarRatingDisplay } from 'react-native-star-rating-widget';
@@ -12,38 +13,52 @@ interface ReviewProps {
   reviewDate: Moment;
 }
 
-const Review = ({ name, profileImage, rating, review, reviewDate }: ReviewProps) => (
-  <View style={reviewStyles.reviewContainer}>
-    <Flex direction="row" justifyContent="space-between">
-      <View style={reviewStyles.avatarContainer}>
-        <Avatar
-          bg={Colors.blueMagenta}
-          source={{
-            uri: profileImage,
-          }}
-        >
-          {/* TODO: Convert user's name to initials */}
-          AJ
-        </Avatar>
-      </View>
+const Review = ({ name, profileImage, rating, review, reviewDate }: ReviewProps) => {
+  const [truncateReview, setTruncateReview] = useState(false);
 
-      <View style={reviewStyles.reviewContentContainer}>
-        <Flex direction="row" justifyContent="space-between">
-          <View>
-            <Text bold>{name}</Text>
+  useEffect(() => {
+    if (review && review.length > 200) {
+      setTruncateReview(true);
+    } else {
+      setTruncateReview(false);
+    }
+  }, [review]);
 
-            <Text bold fontSize="xs" color={Colors.grey}>
-              {reviewDate.format('LL')}
-            </Text>
-          </View>
+  console.log({ review, truncateReview });
 
-          <StarRatingDisplay color={Colors.blueMagenta} rating={rating} starSize={17} />
-        </Flex>
+  return (
+    <View style={reviewStyles.reviewContainer}>
+      <Flex direction="row" justifyContent="space-between">
+        <View style={reviewStyles.avatarContainer}>
+          <Avatar
+            bg={Colors.blueMagenta}
+            source={{
+              uri: profileImage,
+            }}
+          >
+            {/* TODO: Convert user's name to initials */}
+            AJ
+          </Avatar>
+        </View>
 
-        <Text style={reviewStyles.reviewText}>{review}</Text>
-      </View>
-    </Flex>
-  </View>
-);
+        <View style={reviewStyles.reviewContentContainer}>
+          <Flex direction="row" justifyContent="space-between">
+            <View>
+              <Text bold>{name}</Text>
+
+              <Text bold fontSize="xs" color={Colors.grey}>
+                {reviewDate.format('LL')}
+              </Text>
+            </View>
+
+            <StarRatingDisplay color={Colors.blueMagenta} rating={rating} starSize={17} />
+          </Flex>
+
+          <Text style={reviewStyles.reviewText}>{review}</Text>
+        </View>
+      </Flex>
+    </View>
+  );
+};
 
 export default Review;
