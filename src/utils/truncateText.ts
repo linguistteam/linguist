b54b6e1b28581667bd7dbf906e6a0520b06e05db
Jamie McNeil <jamievmcneil@gmail.com>
2022-12-16 17:13:40 -0500

    Change desc for truncateText util

--- src/utils/truncateText.ts
+++ src/utils/truncateText.ts
@@ -1,4 +1,4 @@
-/* Takes text longer than 200 chars and truncates it */
+/* Truncates text longer than 200 chars */
 const truncateText = (shouldTruncate: boolean, text: string) =>
   shouldTruncate ? `${text.substring(0, 200).trim()}...` : text;
 
