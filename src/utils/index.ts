e1035cd86ed208fd7a65400f579ea878ec0f34bb
Jamie McNeil <jamievmcneil@gmail.com>
2022-12-21 11:17:20 -0500

    Add fixedRatingAverage util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export { default as extractInitials } from './extractInitials';
+export { default as fixedRatingAverage } from './fixedRatingAverage';
 export { default as truncateText } from './truncateText';
