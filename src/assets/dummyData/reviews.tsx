4c887087adece565e09fc805315a6658b76be6d0
Jamie McNeil <jamievmcneil@gmail.com>
2022-12-21 08:13:48 -0500

    Remove translator image

--- src/assets/dummyData/reviews.tsx
+++ src/assets/dummyData/reviews.tsx
@@ -142,8 +142,7 @@ const reviews: ReviewsType = {
     },
     {
       userId: '3bc88969-df04-44d7-965a-149590e4bf93',
-      profileImage:
-        'https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+      profileImage: '',
       name: 'Main Doe',
       reviewDate: dummyDateOneWeeksAgo,
       rating: 4.5,
