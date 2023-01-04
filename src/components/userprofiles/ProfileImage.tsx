fec4c8323103227f6428cd536833944550de9093
Jamie McNeil <jamievmcneil@gmail.com>
2023-01-04 08:40:33 -0500

    Adjust Skeleton; Add extracted name initials as backup

--- src/components/userprofiles/ProfileImage.tsx
+++ src/components/userprofiles/ProfileImage.tsx
@@ -1,21 +1,31 @@
 import { Avatar, Skeleton, View } from 'native-base';
+import { extractInitials } from '@utils';
 import { profileImageStyles } from './styles';
 
 interface ProfileImageProps {
+  name: string;
   profileImage: string;
 }
 
-const ProfileImage = ({ profileImage }: ProfileImageProps) => {
+const ProfileImage = ({ name, profileImage }: ProfileImageProps) => {
   // TODO: Should be set based on content loading from DB
-  // TODO: Check to make sure skeleton still looks okay
   const imageIsLoading = false;
 
   return (
     <View>
       {imageIsLoading ? (
-        <Skeleton h="400" />
+        <Skeleton
+          borderWidth={1}
+          borderColor="coolGray.200"
+          endColor="warmGray.50"
+          size="32"
+          rounded="full"
+          marginY={15}
+          alignSelf="center"
+        />
       ) : (
         <Avatar source={{ uri: profileImage }} size="2xl" style={profileImageStyles.profileImage}>
+          {extractInitials(name)}
           <Avatar.Badge bg="green.500" />
         </Avatar>
       )}
