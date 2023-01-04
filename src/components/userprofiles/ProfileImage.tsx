377eac9a1f439e2cba18aed200becb1636936c81
Jamie McNeil <jamievmcneil@gmail.com>
2023-01-04 08:22:09 -0500

    Use Avatar component for profile image

--- src/components/userprofiles/ProfileImage.tsx
+++ src/components/userprofiles/ProfileImage.tsx
@@ -1,14 +1,13 @@
-import { Image, Skeleton, View } from 'native-base';
+import { Avatar, Skeleton, View } from 'native-base';
 import { profileImageStyles } from './styles';
 
 interface ProfileImageProps {
-  imageBlur: boolean;
-  name: string;
   profileImage: string;
 }
 
-const ProfileImage = ({ imageBlur, name, profileImage }: ProfileImageProps) => {
+const ProfileImage = ({ profileImage }: ProfileImageProps) => {
   // TODO: Should be set based on content loading from DB
+  // TODO: Check to make sure skeleton still looks okay
   const imageIsLoading = false;
 
   return (
@@ -16,12 +15,9 @@ const ProfileImage = ({ imageBlur, name, profileImage }: ProfileImageProps) => {
       {imageIsLoading ? (
         <Skeleton h="400" />
       ) : (
-        <Image
-          alt={name}
-          blurRadius={imageBlur ? 10 : 0}
-          source={{ uri: profileImage }}
-          style={profileImageStyles.profileImage}
-        />
+        <Avatar source={{ uri: profileImage }} size="2xl" style={profileImageStyles.profileImage}>
+          <Avatar.Badge bg="green.500" />
+        </Avatar>
       )}
     </View>
   );
