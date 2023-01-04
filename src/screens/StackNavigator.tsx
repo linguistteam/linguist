d25d54a071c5cd19e353411efa05aafa35e07327
Jamie McNeil <jamievmcneil@gmail.com>
2023-01-04 14:55:01 -0500

    Remove unused View

--- src/screens/StackNavigator.tsx
+++ src/screens/StackNavigator.tsx
@@ -1,4 +1,3 @@
-import { View } from 'native-base';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { TranslatorProfile } from '@screens/userprofiles';
