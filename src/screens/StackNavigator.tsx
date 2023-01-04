c1403fe0eefc843da38bccffbe6ccf835f1098dd
Jamie McNeil <jamievmcneil@gmail.com>
2023-01-04 15:25:35 -0500

    Add home to stack nav

--- src/screens/StackNavigator.tsx
+++ src/screens/StackNavigator.tsx
@@ -1,13 +1,15 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from '@screens/Home';
 import { TranslatorProfile } from '@screens/userprofiles';
 
 // TODO: Update import if separating components
 import ClientProfile from '@screens/userprofiles/ClientProfile';
 
 export type StackNavigatorList = {
-  TranslatorProfile: undefined;
+  Home: undefined;
   ClientProfile: undefined;
+  TranslatorProfile: undefined;
 };
 
 const Stack = createNativeStackNavigator<StackNavigatorList>();
@@ -16,14 +18,15 @@ const StackNavigator = () => (
   <NavigationContainer>
     {/* // TODO: Add enums for scree names */}
     <Stack.Navigator
-      initialRouteName="TranslatorProfile"
+      initialRouteName="Home"
       screenOptions={{
         headerShown: false,
         contentStyle: { backgroundColor: '#fff' },
       }}
     >
-      <Stack.Screen name="TranslatorProfile" component={TranslatorProfile} />
+      <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="ClientProfile" component={ClientProfile} />
+      <Stack.Screen name="TranslatorProfile" component={TranslatorProfile} />
     </Stack.Navigator>
   </NavigationContainer>
 );
