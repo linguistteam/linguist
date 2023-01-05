bfc8374b017a27044cc80b6b77da90364ed11516
Jamie McNeil <jamievmcneil@gmail.com>
2023-01-05 15:16:18 -0500

    Add email and password state

--- src/screens/authentication/SignUp.tsx
+++ src/screens/authentication/SignUp.tsx
@@ -1,14 +1,31 @@
+import { useState } from 'react';
 import { Heading, Input, Stack } from 'native-base';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-const SignUp = () => (
-  <SafeAreaView>
-    <Stack space={4} w="75%" maxW="300px" mx="auto" alignItems="center">
-      <Heading size="xl">Sign Up</Heading>
-      <Input variant="outline" placeholder="Email" />
-      <Input variant="outline" placeholder="Password" />
-    </Stack>
-  </SafeAreaView>
-);
+const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <SafeAreaView>
+      <Stack space={4} w="75%" maxW="300px" mx="auto" alignItems="center">
+        <Heading size="xl">Sign Up</Heading>
+        <Input
+          variant="outline"
+          placeholder="Email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          variant="outline"
+          placeholder="Password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          secureTextEntry
+        />
+      </Stack>
+    </SafeAreaView>
+  );
+};
 
 export default SignUp;
