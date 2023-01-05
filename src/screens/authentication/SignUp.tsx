1e129acb9682ed76e45a51dc9198bb61c441f36c
Jamie McNeil <jamievmcneil@gmail.com>
2023-01-05 15:12:29 -0500

    Set up initial sign up/login form

--- src/screens/authentication/SignUp.tsx
+++ src/screens/authentication/SignUp.tsx
@@ -1,5 +1,14 @@
-import { Text } from 'native-base';
+import { Heading, Input, Stack } from 'native-base';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-const SignUp = () => <Text>Sign Up</Text>;
+const SignUp = () => (
+  <SafeAreaView>
+    <Stack space={4} w="75%" maxW="300px" mx="auto" alignItems="center">
+      <Heading size="xl">Sign Up</Heading>
+      <Input variant="outline" placeholder="Email" />
+      <Input variant="outline" placeholder="Password" />
+    </Stack>
+  </SafeAreaView>
+);
 
 export default SignUp;
