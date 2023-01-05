7ce20513fe90bd6345b758dcec836e81ffe2077f
Jamie McNeil <jamievmcneil@gmail.com>
2023-01-05 16:04:55 -0500

    Extract handleSignUp into own util

---
+++ src/utils/handleSignUp.ts
@@ -0,0 +1,22 @@
+import { AuthError, createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../firebaseConfig';
+
+/* Handle signing user up */
+const handleSignUp = (email: string, password: string) => {
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+
+      console.log('sign up user', user);
+    })
+    .catch((error: AuthError) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+
+      console.log('sign up errorCode', errorCode);
+      console.log('sign up errorMessage', errorMessage);
+    });
+};
+
+export default handleSignUp;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 export { default as extractInitials } from './extractInitials';
 export { default as fixedRatingAverage } from './fixedRatingAverage';
 export { default as handleLogin } from './handleLogin';
+export { default as handleSignUp } from './handleSignUp';
 export { default as isLongName } from './isLongName';
 export { default as truncateText } from './truncateText';
