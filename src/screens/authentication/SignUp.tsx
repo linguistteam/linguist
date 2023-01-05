38c8834f630f0e39badaba84290a296ed2c3e6dd
Jamie McNeil <jamievmcneil@gmail.com>
2023-01-05 16:05:13 -0500

    Remove login and sign up funcs; Import as utils

--- src/screens/authentication/SignUp.tsx
+++ src/screens/authentication/SignUp.tsx
@@ -1,51 +1,14 @@
 import { useState } from 'react';
 import { Heading, Input, Stack } from 'native-base';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import {
-  AuthError,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-} from 'firebase/auth';
-import { auth } from '../../../firebaseConfig';
 import { Button } from '@common';
+import { handleLogin, handleSignUp } from '@utils';
 
 const SignUp = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleLogin = () => {
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        console.log('logged in user', user);
-      })
-      .catch((error: AuthError) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-
-        console.log('log in errorCode', errorCode);
-        console.log('log in errorMessage', errorMessage);
-      });
-  };
-
-  const handleSignUp = () => {
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-
-        console.log('sign up user', user);
-      })
-      .catch((error: AuthError) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-
-        console.log('sign up errorCode', errorCode);
-        console.log('sign up errorMessage', errorMessage);
-      });
-  };
-
+  // TODO: Add all text to strings file
   return (
     <SafeAreaView>
       <Stack space={4} w="75%" maxW="300px" mx="auto" alignItems="center">
@@ -63,8 +26,8 @@ const SignUp = () => {
           onChangeText={(text) => setPassword(text)}
           secureTextEntry
         />
-        <Button onPress={() => handleLogin()} text="Log In" width="100%" />
-        <Button onPress={() => handleSignUp()} text="Sign Up" width="100%" />
+        <Button onPress={() => handleLogin(email, password)} text="Log In" width="100%" />
+        <Button onPress={() => handleSignUp(email, password)} text="Sign Up" width="100%" />
       </Stack>
     </SafeAreaView>
   );
