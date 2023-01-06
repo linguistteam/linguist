d2ef83b88e6950acb9d5fc8281ebce58290757ed
Jamie McNeil <jamievmcneil@gmail.com>
2023-01-06 09:00:12 -0500

    Add stack for buttons; Add sign out button

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,4 @@
-import { Heading } from 'native-base';
+import { Heading, Stack } from 'native-base';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
 import { Button } from '@common';
@@ -8,14 +8,20 @@ import { auth } from '../../firebaseConfig';
 
 const Home = () => {
   const navigation = useNavigation<NativeStackNavigationProp<StackNavigatorList>>();
-  console.log('auth', auth.currentUser?.email);
 
   return (
     <SafeAreaView>
       <Heading size="md">Logged in as: {auth.currentUser?.email}</Heading>
-      <Button onPress={() => navigation.navigate('SIGN_UP')} text="Sign Up" />
-      <Button onPress={() => navigation.navigate('TRANSLATOR_PROFILE')} text="Translator Profile" />
-      <Button onPress={() => navigation.navigate('CLIENT_PROFILE')} text="Client Profile" />
+
+      <Stack space={4} w="75%" maxW="300px" mx="auto" alignItems="center">
+        <Button onPress={() => navigation.navigate('SIGN_UP')} text="Sign Up" />
+        <Button
+          onPress={() => navigation.navigate('TRANSLATOR_PROFILE')}
+          text="Translator Profile"
+        />
+        <Button onPress={() => navigation.navigate('CLIENT_PROFILE')} text="Client Profile" />
+        <Button onPress={() => console.log('handle sign out')} text="Sign Out" />
+      </Stack>
     </SafeAreaView>
   );
 };
