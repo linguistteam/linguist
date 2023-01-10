b28f6d7bee7672c6e51f444455b02902eff605f5
Jamie McNeil <jamievmcneil@gmail.com>
2023-01-10 16:24:21 -0500

    Revert useCheckLoggedInState

--- src/utils/useCheckLoggedInState.ts
+++ src/utils/useCheckLoggedInState.ts
@@ -2,18 +2,15 @@ import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { StackNavigatorList } from '@screens/StackNavigator';
 import { auth } from '../../firebaseConfig';
-import { useUserStore } from '@stores/user';
 
 // TODO: Cleanup logs/Polish
 /* Handle checking if user is logged in */
 /* Learn more about Firebase Auth: https://firebase.google.com/docs/auth/web/start */
 const useCheckLoggedInState = () => {
   const navigation = useNavigation<NativeStackNavigationProp<StackNavigatorList>>();
-  const user = useUserStore((state) => state.user);
 
-  auth.onAuthStateChanged(() => {
-    // TODO: Figure out accurate check of if user is logged in
-    if (user.email) {
+  auth.onAuthStateChanged((user) => {
+    if (user) {
       console.log('user logged in');
       navigation.replace('HOME');
     } else {
