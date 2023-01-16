70bfd9520de9fac8f1853bd3a04757862dc43fcd
Jamie McNeil <jamievmcneil@gmail.com>
2023-01-16 17:50:14 -0500

    Add new bg props for button

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -4,25 +4,29 @@ import { Button as NativeBaseButton, Text } from 'native-base';
 import Colors from '@assets/colors';
 
 interface ButtonProps {
+  bgColor?: string;
   onPress: (event: GestureResponderEvent) => void;
+  pressedBgColor?: string;
   text: string;
+  // textTransform?: string;
   width?: number | string;
 }
 
-const Button = ({ onPress, text, width }: ButtonProps) => {
+const Button = ({ bgColor, onPress, pressedBgColor, text, width }: ButtonProps) => {
   const [isPressed, setIsPressed] = useState(false);
 
   return (
     <NativeBaseButton
       onPress={onPress}
-      bgColor={isPressed ? Colors.lightenedBlueMagenta : Colors.blueMagenta}
+      bgColor={isPressed ? pressedBgColor : bgColor}
+      // bgColor={isPressed ? Colors.lightenedBlueMagenta : Colors.blueMagenta}
       borderRadius={20}
       height={45}
       width={width}
       onPressIn={() => setIsPressed(true)}
       onPressOut={() => setIsPressed(false)}
     >
-      <Text color={Colors.white} fontSize={15} fontWeight={700} textTransform="uppercase">
+      <Text color={Colors.white} fontSize={15} fontWeight={700}>
         {text}
       </Text>
     </NativeBaseButton>
