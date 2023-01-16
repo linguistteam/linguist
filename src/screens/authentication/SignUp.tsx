da5fcb6b21d644372cf959b7036250451479270d
Jamie McNeil <jamievmcneil@gmail.com>
2023-01-16 17:22:32 -0500

    Add Hello string

--- src/screens/authentication/SignUp.tsx
+++ src/screens/authentication/SignUp.tsx
@@ -4,6 +4,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { Button } from '@common';
 import { handleSignUp, useCheckLoggedInState } from '@utils';
 import { useUserStore } from '@stores/user';
+import { EN } from '@assets/strings';
 
 const SignUp = () => {
   const [email, setEmail] = useState('');
@@ -16,7 +17,7 @@ const SignUp = () => {
   return (
     <SafeAreaView>
       <Stack space={4} w="75%" maxW="300px" mx="auto" alignItems="center">
-        <Heading size="xl">Sign Up</Heading>
+        <Heading size="xl">{EN.COMMON.HELLO}</Heading>
         <Input
           variant="outline"
           placeholder="Email"
