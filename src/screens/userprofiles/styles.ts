3f79be50e0bd321ce7bdcc4998fe4192d78df9b7
Jamie McNeil <jamievmcneil@gmail.com>
2023-01-16 18:30:28 -0500

    TODO: Create app wide container

--- src/screens/userprofiles/styles.ts
+++ src/screens/userprofiles/styles.ts
@@ -3,6 +3,7 @@ import { StyleSheet } from 'react-native';
 export const profileStyles = StyleSheet.create({
   profileContent: {
     backgroundColor: '#ffffff',
+    // TODO: Create app wide container?
     paddingHorizontal: 20,
   },
   hireButton: {
