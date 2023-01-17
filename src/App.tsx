c6c193a89b2123c0b6d22f591e39b14213e18d61
Jamie McNeil <jamievmcneil@gmail.com>
2023-01-17 08:11:50 -0500

    Add global app container

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NativeBaseProvider } from 'native-base';
 import { StatusBar } from 'expo-status-bar';
 import StackNavigator from '@screens/StackNavigator';
+import { globalStyles } from '@constants/styles';
 
 // TODO: Set up @react-navigation here. Docs: https://reactnative.dev/docs/navigation
 export default function App() {
   return (
     <NativeBaseProvider>
-      <SafeAreaProvider>
+      <SafeAreaProvider style={globalStyles.appContainer}>
         {/* TODO: Change status bar color based on theme */}
         <StatusBar style="dark" />
         <StackNavigator />
