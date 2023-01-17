e00de3d331c64c53e93ca37716cfbe014ffb2ed4
Jamie McNeil <jamievmcneil@gmail.com>
2023-01-17 09:36:29 -0500

    Pass theme to NativeBaseProvider

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import { NativeBaseProvider } from 'native-base';
 import { StatusBar } from 'expo-status-bar';
 import StackNavigator from '@screens/StackNavigator';
 import { globalStyles } from '@constants/styles';
+import theme from '@constants/theme';
 
 // TODO: Set up @react-navigation here. Docs: https://reactnative.dev/docs/navigation
 export default function App() {
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={theme}>
       <SafeAreaProvider style={globalStyles.appContainer}>
         {/* TODO: Change status bar color based on theme */}
         <StatusBar style="dark" />
