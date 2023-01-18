eb7934ac9c4026d7ac8dbdf636e76d23edd3730c
Jamie McNeil <jamievmcneil@gmail.com>
2023-01-18 11:08:47 -0500

    Remove TODO: Set up @react-navigation here; Import and use config

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NativeBaseProvider } from 'native-base';
 import { StatusBar } from 'expo-status-bar';
 import StackNavigator from '@screens/StackNavigator';
-import { globalStyles } from '@constants/styles';
+import config from '@constants/config';
 import theme from '@constants/theme';
 
-// TODO: Set up @react-navigation here. Docs: https://reactnative.dev/docs/navigation
 export default function App() {
   return (
-    <NativeBaseProvider theme={theme}>
-      <SafeAreaProvider style={globalStyles.appContainer}>
+    <NativeBaseProvider config={config} theme={theme}>
+      <SafeAreaProvider>
         {/* TODO: Change status bar color based on theme */}
         <StatusBar style="dark" />
         <StackNavigator />
