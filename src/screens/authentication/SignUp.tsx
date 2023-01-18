04a1dd5a991e77074697bf43f8ac3d1298f8a1ab
Jamie McNeil <jamievmcneil@gmail.com>
2023-01-18 11:45:32 -0500

    Apply latest styles

--- src/screens/authentication/SignUp.tsx
+++ src/screens/authentication/SignUp.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Button, Heading, Input, Pressable, Text, View } from 'native-base';
+import { Box, Button, Heading, Input, Pressable, Text, View } from 'native-base';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
@@ -9,7 +9,9 @@ import { handleSignUp, useCheckLoggedInState } from '@utils';
 import { useUserStore } from '@stores/user';
 import { EN } from '@assets/strings';
 import Colors from '@assets/colors';
+import { globalStyles } from '@constants/styles';
 import { StackNavigatorList } from '@screens/StackNavigator';
+import { authenticationStyles } from './styles';
 
 const SignUp = () => {
   const navigation = useNavigation<NativeStackNavigationProp<StackNavigatorList>>();
@@ -24,81 +26,87 @@ const SignUp = () => {
 
   return (
     <SafeAreaView>
-      <View justifyContent="center" height="100%">
-        <View marginBottom={8}>
-          <Heading size="xl" textAlign="center">
-            {EN.COMMON.HELLO}
-          </Heading>
-          <Heading size="lg" textAlign="center">
-            {EN.SIGN_UP.SUBHEADING}
-          </Heading>
-        </View>
+      <Box
+        bg={{
+          linearGradient: {
+            colors: [Colors.yellowLightened80, Colors.blueMagentaLightened80],
+            start: [0, 0],
+            end: [1, 1],
+          },
+        }}
+        width="100%"
+      >
+        <View style={globalStyles.appContainer}>
+          <View justifyContent="center" height="100%">
+            <View marginBottom={8}>
+              <Heading size="xl" textAlign="center" style={authenticationStyles.textShadow}>
+                {EN.COMMON.HELLO}
+              </Heading>
+              <Heading size="lg" textAlign="center" style={authenticationStyles.textShadow}>
+                {EN.SIGN_UP.SUBHEADING}
+              </Heading>
+            </View>
 
-        <View marginBottom={4}>
-          <Button
-            variant="magenta"
-            leftIcon={<FontAwesomeIcon name="google" color={Colors.white} size={15} />}
-            onPress={() => console.log('Handle Google login')}
-          >
-            {EN.COMMON.CONTINUE_WITH_GOOGLE}
-          </Button>
-        </View>
-        {showEmailForm && (
-          <View marginBottom={4}>
-            <Input
-              variant="outline"
-              placeholder={EN.COMMON.EMAIL_ADDRESS}
-              value={email}
-              onChangeText={(text) => setEmail(text)}
-              type="text"
-              marginBottom={3}
-            />
-            <Input
-              variant="outline"
-              placeholder={EN.COMMON.PASSWORD}
-              value={password}
-              onChangeText={(text) => setPassword(text)}
-              InputRightElement={
-                <Pressable onPress={() => setShowPassword(!showPassword)} marginRight={2}>
-                  <IoniconsIcon
-                    name={showPassword ? 'eye-off-outline' : 'eye-outline'}
-                    size={20}
-                    color={Colors.grey}
-                  />
-                </Pressable>
+            <View marginBottom={4}>
+              <Button
+                variant="magenta"
+                leftIcon={<FontAwesomeIcon name="google" color={Colors.white} size={15} />}
+                onPress={() => console.log('Handle Google login')}
+                shadow={0}
+              >
+                {EN.SIGN_UP.CREATE_ACCOUNT_WITH_GOOGLE}
+              </Button>
+            </View>
+            {showEmailForm && (
+              <View marginBottom={4}>
+                <Input
+                  variant="outline"
+                  placeholder={EN.COMMON.EMAIL_ADDRESS}
+                  value={email}
+                  onChangeText={(text) => setEmail(text)}
+                  type="text"
+                  marginBottom={3}
+                />
+                <Input
+                  variant="outline"
+                  placeholder={EN.COMMON.PASSWORD}
+                  value={password}
+                  onChangeText={(text) => setPassword(text)}
+                  InputRightElement={
+                    <Pressable onPress={() => setShowPassword(!showPassword)} marginRight={2}>
+                      <IoniconsIcon
+                        name={showPassword ? 'eye-off-outline' : 'eye-outline'}
+                        size={20}
+                        color={Colors.grey}
+                      />
+                    </Pressable>
+                  }
+                  type={showPassword ? 'text' : 'password'}
+                  marginBottom={1}
+                />
+              </View>
+            )}
+            <Button
+              variant="grey"
+              onPress={() =>
+                showEmailForm ? handleSignUp({ email, password, setUser }) : setShowEmailForm(true)
               }
-              type={showPassword ? 'text' : 'password'}
-              marginBottom={1}
-            />
+              shadow={0}
+            >
+              {EN.SIGN_UP.CREATE_ACCOUNT_WITH_EMAIL}
+            </Button>
 
             <Text
               color={Colors.blueMagenta}
-              onPress={() => console.log('Handle user forgot password')}
-              fontSize="xs"
-              textAlign="right"
+              onPress={() => navigation.navigate('LOG_IN')}
+              textAlign="center"
+              marginTop={3}
             >
-              {EN.LOG_IN.FORGOT_PASSWORD}
+              {EN.SIGN_UP.LOGIN_INSTEAD}
             </Text>
           </View>
-        )}
-        <Button
-          variant="grey"
-          onPress={() =>
-            showEmailForm ? handleSignUp({ email, password, setUser }) : setShowEmailForm(true)
-          }
-        >
-          {EN.COMMON.CONTINUE_WITH_EMAIL}
-        </Button>
-
-        <Text
-          color={Colors.blueMagenta}
-          onPress={() => navigation.navigate('LOG_IN')}
-          textAlign="center"
-          marginTop={3}
-        >
-          {EN.SIGN_UP.LOGIN_INSTEAD}
-        </Text>
-      </View>
+        </View>
+      </Box>
     </SafeAreaView>
   );
 };
