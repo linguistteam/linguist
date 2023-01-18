683a5e0530b73a23fc648e2b04ca191b5e953658
Jamie McNeil <jamievmcneil@gmail.com>
2023-01-18 11:09:13 -0500

    Add warm gray for input focus color

--- src/constants/theme.tsx
+++ src/constants/theme.tsx
@@ -2,47 +2,47 @@ import { extendTheme } from 'native-base';
 import Colors from '@assets/colors';
 
 const theme = extendTheme({
-  components: {
-    Button: {
-      variants: {
-        magenta: {
-          bg: Colors.blueMagenta,
-          borderRadius: 20,
-          height: 45,
-          _text: {
-            color: Colors.white,
-            fontWeight: '700',
-          },
-          _pressed: {
-            bg: Colors.blueMagentaLightened20,
-          },
+    components: {
+        Button: {
+            variants: {
+                magenta: {
+                    bg: Colors.blueMagenta,
+                    borderRadius: 20,
+                    height: 45,
+                    _text: {
+                        color: Colors.white,
+                        fontWeight: '700',
+                    },
+                    _pressed: {
+                        bg: Colors.blueMagentaLightened20,
+                    },
+                },
+                grey: {
+                    bg: Colors.grey,
+                    borderRadius: 20,
+                    height: 45,
+                    _text: {
+                        color: Colors.white,
+                        fontWeight: '700',
+                    },
+                    _pressed: {
+                        bg: Colors.lightenedGrey,
+                    },
+                },
+            },
         },
-        grey: {
-          bg: Colors.grey,
-          borderRadius: 20,
-          height: 45,
-          _text: {
-            color: Colors.white,
-            fontWeight: '700',
-          },
-          _pressed: {
-            bg: Colors.lightenedGrey,
-          },
-        },
-      },
-    },
-    Input: {
-      baseStyle: {
-        borderColor: Colors.grey,
-        placeholderTextColor: Colors.grey,
+        Input: {
+            baseStyle: {
+                borderColor: Colors.grey,
+                placeholderTextColor: Colors.grey,
 
-        _focus: {
-          backgroundColor: Colors.blueMagentaLightened80,
-          borderColor: Colors.blueMagenta,
+                _focus: {
+                    backgroundColor: 'warmGray.200',
+                    borderColor: Colors.blueMagenta,
+                },
+            },
         },
-      },
     },
-  },
 });
 
 export default theme;
