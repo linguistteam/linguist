1484ba5acf631239896a53f748ac0800238908e6
Jamie McNeil <jamievmcneil@gmail.com>
2023-01-18 11:34:38 -0500

    Fix indents

--- src/constants/theme.tsx
+++ src/constants/theme.tsx
@@ -2,47 +2,47 @@ import { extendTheme } from 'native-base';
 import Colors from '@assets/colors';
 
 const theme = extendTheme({
-    components: {
-        Button: {
-            variants: {
-                magenta: {
-                    bg: Colors.blueMagenta,
-                    borderRadius: 20,
-                    height: 45,
-                    _text: {
-                        color: Colors.white,
-                        fontWeight: '700',
-                    },
-                    _pressed: {
-                        bg: Colors.blueMagentaLightened20,
-                    },
-                },
-                grey: {
-                    bg: Colors.grey,
-                    borderRadius: 20,
-                    height: 45,
-                    _text: {
-                        color: Colors.white,
-                        fontWeight: '700',
-                    },
-                    _pressed: {
-                        bg: Colors.lightenedGrey,
-                    },
-                },
-            },
+  components: {
+    Button: {
+      variants: {
+        magenta: {
+          bg: Colors.blueMagenta,
+          borderRadius: 20,
+          height: 45,
+          _text: {
+            color: Colors.white,
+            fontWeight: '700',
+          },
+          _pressed: {
+            bg: Colors.blueMagentaLightened20,
+          },
         },
-        Input: {
-            baseStyle: {
-                borderColor: Colors.grey,
-                placeholderTextColor: Colors.grey,
-
-                _focus: {
-                    backgroundColor: 'warmGray.200',
-                    borderColor: Colors.blueMagenta,
-                },
-            },
+        grey: {
+          bg: Colors.grey,
+          borderRadius: 20,
+          height: 45,
+          _text: {
+            color: Colors.white,
+            fontWeight: '700',
+          },
+          _pressed: {
+            bg: Colors.lightenedGrey,
+          },
         },
+      },
     },
+    Input: {
+      baseStyle: {
+        borderColor: Colors.grey,
+        placeholderTextColor: Colors.grey,
+
+        _focus: {
+          backgroundColor: 'warmGray.200',
+          borderColor: Colors.blueMagenta,
+        },
+      },
+    },
+  },
 });
 
 export default theme;
