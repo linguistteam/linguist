c120569cca48c550452af3b0a20f40234892ac50
Jamie McNeil <jamievmcneil@gmail.com>
2023-01-24 09:37:47 -0500

    Add error message and code to store

--- src/stores/errors/authError.ts
+++ src/stores/errors/authError.ts
@@ -3,24 +3,26 @@ import { createJSONStorage, persist } from 'zustand/middleware';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export interface FirebaseAuthError {
-  error: string;
+  errorMessage: string;
+  errorCode: string;
 }
 
 export interface FirebaseAuthErrorState {
-  error: string;
-  setError: ({ error }: FirebaseAuthError) => void;
+  error: FirebaseAuthError;
+  setError: ({ errorMessage, errorCode }: FirebaseAuthError) => void;
   reset: () => void;
 }
 
 const initialState = {
-  error: '',
+  error: { errorMessage: '', errorCode: '' },
 };
 
 export const useAuthErrorStore = create<FirebaseAuthErrorState>()(
   persist(
     (set) => ({
       ...initialState,
-      setError: ({ error }) => set(() => ({ error })),
+      setError: ({ errorMessage, errorCode }) =>
+        set(() => ({ error: { errorMessage, errorCode } })),
 
       reset: () => {
         set(initialState);
