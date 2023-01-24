8e05a3d41fc4faa3261541d1f6c733521f39d317
Jamie McNeil <jamievmcneil@gmail.com>
2023-01-24 09:58:50 -0500

    Remove persist error state

--- src/stores/errors/authError.ts
+++ src/stores/errors/authError.ts
@@ -1,6 +1,4 @@
 import create from 'zustand';
-import { createJSONStorage, persist } from 'zustand/middleware';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export interface FirebaseAuthError {
   errorMessage: string;
@@ -17,20 +15,11 @@ const initialState = {
   error: { errorMessage: '', errorCode: '' },
 };
 
-export const useAuthErrorStore = create<FirebaseAuthErrorState>()(
-  persist(
-    (set) => ({
-      ...initialState,
-      setError: ({ errorMessage, errorCode }) =>
-        set(() => ({ error: { errorMessage, errorCode } })),
+export const useAuthErrorStore = create<FirebaseAuthErrorState>()((set) => ({
+  ...initialState,
+  setError: ({ errorMessage, errorCode }) => set(() => ({ error: { errorMessage, errorCode } })),
 
-      reset: () => {
-        set(initialState);
-      },
-    }),
-    {
-      name: 'auth-error-storage',
-      storage: createJSONStorage(() => AsyncStorage),
-    },
-  ),
-);
+  reset: () => {
+    set(initialState);
+  },
+}));
