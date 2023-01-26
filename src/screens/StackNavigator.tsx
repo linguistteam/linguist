9e3670aa7d840521b3d93d0126001c6b103fbccd
Jamie McNeil <jamievmcneil@gmail.com>
2023-01-26 16:03:55 -0500

    Add authenticated routes

--- src/screens/StackNavigator.tsx
+++ src/screens/StackNavigator.tsx
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from '@screens/Home';
 import { Authentication } from '@screens/authentication';
 import { ClientProfile, TranslatorProfile } from '@screens/userprofiles';
+import { useUserStore } from '@stores/user';
 
 // NOTE: Specifying undefined means that the route doesn't have params
 // More info/types here: https://reactnavigation.org/docs/typescript/
@@ -16,21 +17,33 @@ export type StackNavigatorList = {
 
 const Stack = createNativeStackNavigator<StackNavigatorList>();
 
-const StackNavigator = () => (
-  <NavigationContainer>
-    <Stack.Navigator
-      initialRouteName="AUTHENTICATION"
-      screenOptions={{
-        headerShown: false,
-        contentStyle: { backgroundColor: '#fff' },
-      }}
-    >
-      <Stack.Screen name="HOME" component={Home} />
-      <Stack.Screen name="CLIENT_PROFILE" component={ClientProfile} />
-      <Stack.Screen name="AUTHENTICATION" component={Authentication} />
-      <Stack.Screen name="TRANSLATOR_PROFILE" component={TranslatorProfile} />
-    </Stack.Navigator>
-  </NavigationContainer>
-);
+const StackNavigator = () => {
+  const user = useUserStore((state) => state.user);
+  const isLoggedIn = user.uid;
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: '#fff' },
+        }}
+      >
+        {/* TODO: See if adding loading spinner fixes ERROR: The action
+        'REPLACE' with payload {"name":"HOME"} was not handled by any
+        navigator. */}
+        {isLoggedIn ? (
+          <>
+            <Stack.Screen name="HOME" component={Home} />
+            <Stack.Screen name="CLIENT_PROFILE" component={ClientProfile} />
+            <Stack.Screen name="TRANSLATOR_PROFILE" component={TranslatorProfile} />
+          </>
+        ) : (
+          <Stack.Screen name="AUTHENTICATION" component={Authentication} />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default StackNavigator;
