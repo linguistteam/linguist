e190d99a0a35626f77438304c2cb38e055f28a53
Jamie McNeil <jamievmcneil@gmail.com>
2023-01-27 11:30:14 -0500

    Add setError and setLoading handling to logout

--- src/utils/handleLogout.ts
+++ src/utils/handleLogout.ts
@@ -1,21 +1,31 @@
 import { AuthError } from 'firebase/auth';
 import { auth } from '../../firebaseConfig';
+import { FirebaseAuthError } from '@stores/errors/authError';
+import { Loading } from '@stores/loading';
+import mapFirebaseAuthErrors from './mapFirebaseAuthErrors';
 
 /* Handle logging out user */
 /* Learn more about Firebase Auth: https://firebase.google.com/docs/auth/web/password-auth */
 
 interface HandleLogoutProps {
   resetUser: () => void;
+  setError: ({ errorMessage, errorCode }: FirebaseAuthError) => void;
+  setLoading: ({ isLoading }: Loading) => void;
 }
 
-const handleLogout = ({ resetUser }: HandleLogoutProps) => {
+const handleLogout = ({ resetUser, setError, setLoading }: HandleLogoutProps) => {
+  setLoading({ isLoading: true });
+
   auth
     .signOut()
     .then(() => {
       resetUser();
+      setLoading({ isLoading: false });
     })
     .catch((error: AuthError) => {
-      console.error('The following error has occurred: ', error.message);
+      console.error('The following error has occurred: ', error.code);
+      setError({ errorMessage: mapFirebaseAuthErrors(error.code), errorCode: error.code });
+      setLoading({ isLoading: false });
     });
 };
 
