63a0329b38acd897cf082b7924947a142d7601cb
Jamie McNeil <jamievmcneil@gmail.com>
2023-01-27 09:21:28 -0500

    Remove log; Remove TODO: Cleanup logs/Polish

--- src/utils/handleLogout.ts
+++ src/utils/handleLogout.ts
@@ -1,7 +1,6 @@
 import { AuthError } from 'firebase/auth';
 import { auth } from '../../firebaseConfig';
 
-// TODO: Cleanup logs/Polish
 /* Handle logging out user */
 /* Learn more about Firebase Auth: https://firebase.google.com/docs/auth/web/password-auth */
 
@@ -14,7 +13,6 @@ const handleLogout = ({ resetUser }: HandleLogoutProps) => {
     .signOut()
     .then(() => {
       resetUser();
-      console.log('user is logging out...');
     })
     .catch((error: AuthError) => {
       console.error('The following error has occurred: ', error.message);
