d2469a79dd86c51d33aff29429b63794d9d75ed2
Jamie McNeil <jamievmcneil@gmail.com>
2023-01-27 11:21:08 -0500

    Remove todos/logs; Add setloading state

--- src/utils/handleSignUp.ts
+++ src/utils/handleSignUp.ts
@@ -2,6 +2,7 @@ import { AuthError, createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../firebaseConfig';
 import { User } from '@stores/user';
 import { FirebaseAuthError } from '@stores/errors/authError';
+import { Loading } from '@stores/loading';
 import mapFirebaseAuthErrors from './mapFirebaseAuthErrors';
 
 /* Handle signing user up */
@@ -11,25 +12,26 @@ interface HandleSignUpProps {
   password: string;
   setUser: ({ email, uid }: User) => void;
   setError: ({ errorMessage, errorCode }: FirebaseAuthError) => void;
+  setLoading: ({ isLoading }: Loading) => void;
 }
 
-// TODO: Cleanup logs/Polish
-// TODO: Add validation (i.e. don't allow empty form, invalid email, etc)
-const handleSignUp = ({ email, password, setUser, setError }: HandleSignUpProps) => {
+const handleSignUp = ({ email, password, setUser, setError, setLoading }: HandleSignUpProps) => {
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-      // Signed in
+      setLoading({ isLoading: true });
       // TODO: If !email, throw some error and don't log user in
       const user = userCredential.user;
       const { email, uid } = user;
       const userEmail = email ?? '';
 
-      console.log({ email, uid });
       setUser({ email: userEmail, uid });
+
+      setLoading({ isLoading: false });
     })
     .catch((error: AuthError) => {
       console.error('The following error has occurred: ', error.code);
       setError({ errorMessage: mapFirebaseAuthErrors(error.code), errorCode: error.code });
+      setLoading({ isLoading: false });
     });
 };
 
