e4c880d5923e5dee021ed31770b235b73df662c4
Jamie McNeil <jamievmcneil@gmail.com>
2023-01-30 12:08:35 -0500

    Pass user email to handleSendPasswordResetEmail

--- src/utils/handleSendPasswordResetEmail.ts
+++ src/utils/handleSendPasswordResetEmail.ts
@@ -7,18 +7,19 @@ import mapFirebaseAuthErrors from './mapFirebaseAuthErrors';
 /* Learn more about Firebase Auth: https://firebase.google.com/docs/auth/web/manage-users#send_a_password_reset_email */
 
 interface HandleSendPasswordResetEmailProps {
-    setError: ({ errorMessage, errorCode }: FirebaseAuthError) => void;
+  email: string;
+  setError: ({ errorMessage, errorCode }: FirebaseAuthError) => void;
 }
 
-const handleSendPasswordResetEmail = ({ setError }: HandleSendPasswordResetEmailProps) => {
-    sendPasswordResetEmail(auth, 'jamievmcneil@gmail.com')
-        .then(() => {
-            console.log('email sent!');
-        })
-        .catch((error: AuthError) => {
-            console.error('The following error has occurred: ', error.code);
-            setError({ errorMessage: mapFirebaseAuthErrors(error.code), errorCode: error.code });
-        });
+const handleSendPasswordResetEmail = ({ email, setError }: HandleSendPasswordResetEmailProps) => {
+  sendPasswordResetEmail(auth, email)
+    .then(() => {
+      console.log('email sent!');
+    })
+    .catch((error: AuthError) => {
+      console.error('The following error has occurred: ', error.code);
+      setError({ errorMessage: mapFirebaseAuthErrors(error.code), errorCode: error.code });
+    });
 };
 
 export default handleSendPasswordResetEmail;
