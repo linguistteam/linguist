017e3919c42c075be015efa9626f54947b55f42e
Jamie McNeil <jamievmcneil@gmail.com>
2023-01-31 10:10:45 -0500

    Add pw reset types

--- src/screens/authentication/types.ts
+++ src/screens/authentication/types.ts
@@ -1,5 +1,16 @@
+import { StackNavigatorList } from '@screens/StackNavigator';
+
 export interface FormErrors {
   email: boolean;
   general: boolean;
   password: boolean;
+}
+
+export interface PasswordResetRouteParams {
+  passwordReset: boolean;
+}
+
+export interface PasswordResetNavigate {
+  name: StackNavigatorList;
+  params: PasswordResetRouteParams;
 }
