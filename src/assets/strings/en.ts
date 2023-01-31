3bdfba63f420724897a4a5186ba26cf4c9462a51
Jamie McNeil <jamievmcneil@gmail.com>
2023-01-31 11:34:43 -0500

    Add RESET_LINK_SENT string

--- src/assets/strings/en.ts
+++ src/assets/strings/en.ts
@@ -40,6 +40,7 @@ export const EN = {
     ENTER_EMAIL_ADDRESS:
       'Enter the email address associated with your account, and we’ll email you a link to reset your password.',
     SEND_RESET_LINK: 'Send reset link',
+    RESET_LINK_SENT: 'A link to reset your password has been sent to test@email.com.',
   },
   REVIEWS: {
     FROM_CLIENTS: 'From clients',
