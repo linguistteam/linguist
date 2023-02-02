69e87cc4137b3d0e612649aad2c54c3b80a7011b
Jamie McNeil <jamievmcneil@gmail.com>
2023-02-02 10:53:19 -0500

    Add null type for photoURL

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -5,7 +5,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 export interface User {
   displayName: string | null;
   email: string | null;
-  photoURL: string;
+  photoURL: string | null;
   uid: string;
 }
 
