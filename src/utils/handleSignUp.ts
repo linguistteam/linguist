9607f6c3d0c5bb2fd693b8b02d2e619eba788211
Jamie McNeil <jamievmcneil@gmail.com>
2023-02-02 08:38:13 -0500

    Add updateProfile call to sign up; Pass in display name

--- src/utils/handleSignUp.ts
+++ src/utils/handleSignUp.ts
@@ -1,4 +1,4 @@
-import { AuthError, createUserWithEmailAndPassword } from 'firebase/auth';
+import { AuthError, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { auth } from '../../firebaseConfig';
 import { FirebaseAuthError } from '@stores/errors/authError';
 import { Loading } from '@stores/loading';
@@ -8,18 +8,35 @@ import mapFirebaseAuthErrors from './mapFirebaseAuthErrors';
 /* Learn more about Firebase Auth: https://firebase.google.com/docs/auth/web/start */
 
 interface HandleSignUpProps {
+  displayName: string;
   email: string;
   password: string;
   setError: ({ errorMessage, errorCode }: FirebaseAuthError) => void;
   setLoading: ({ isLoading }: Loading) => void;
 }
 
-const handleSignUp = ({ email, password, setError, setLoading }: HandleSignUpProps) => {
+const handleSignUp = ({
+  displayName,
+  email,
+  password,
+  setError,
+  setLoading,
+}: HandleSignUpProps) => {
   setLoading({ isLoading: true });
 
   createUserWithEmailAndPassword(auth, email, password)
     .then(() => {
-      setLoading({ isLoading: false });
+      updateProfile(auth.currentUser, {
+        displayName,
+      })
+        .then(() => {
+          setLoading({ isLoading: false });
+        })
+        .catch((error: AuthError) => {
+          console.error('The following error has occurred: ', error.code);
+          setError({ errorMessage: mapFirebaseAuthErrors(error.code), errorCode: error.code });
+          setLoading({ isLoading: false });
+        });
     })
     .catch((error: AuthError) => {
       console.error('The following error has occurred: ', error.code);
