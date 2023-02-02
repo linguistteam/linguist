6636649d36563e72f40a9f863bdd57afe3106c4f
Jamie McNeil <jamievmcneil@gmail.com>
2023-02-02 10:44:33 -0500

    Add button to update profile image; Show image

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -6,7 +6,8 @@ import { StackNavigatorList } from './StackNavigator';
 import { useUserStore } from '@stores/user';
 import { useAuthErrorStore } from '@stores/errors/authError';
 import { useLoadingStore } from '@stores/loading';
-import { handleLogout } from '@utils';
+import { handleLogout, handleUpdateProfilePhoto } from '@utils';
+import { ProfileImage } from '@components/userprofiles';
 
 const Home = () => {
   const navigation = useNavigation<NativeStackNavigationProp<StackNavigatorList, 'HOME'>>();
@@ -15,12 +16,19 @@ const Home = () => {
   const setLoading = useLoadingStore((state) => state.setLoading);
   const resetUser = useUserStore((state) => state.reset);
 
+  const photoURL =
+    'https://images.unsplash.com/photo-1664575600850-c4b712e6e2bf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80';
+
   return (
     <SafeAreaView>
       <Heading size="md">Logged in as: {user.displayName}</Heading>
       <Heading size="md">Email: {user.email}</Heading>
+      <ProfileImage name="Test User" profileImage={photoURL} />
 
       <Stack space={4} w="75%" maxW="300px" mx="auto" alignItems="center">
+        <Button onPress={() => handleUpdateProfilePhoto({ photoURL, setError, setLoading })}>
+          Update profile photo
+        </Button>
         <Button onPress={() => navigation.navigate('TRANSLATOR_PROFILE')}>
           Translator Profile
         </Button>
