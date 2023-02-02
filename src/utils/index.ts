4352faaf30e24bbd7043692648894756763a7af2
Jamie McNeil <jamievmcneil@gmail.com>
2023-02-02 10:45:14 -0500

    Add handleUpdateProfilePhoto; Alphabetize

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,11 @@
 // Authentication
+export { default as handleCheckLoggedInState } from './authentication/handleCheckLoggedInState';
 export { default as handleLogin } from './authentication/handleLogin';
 export { default as handleLogout } from './authentication/handleLogout';
 export { default as handleSendPasswordResetEmail } from './authentication/handleSendPasswordResetEmail';
 export { default as handleSignUp } from './authentication/handleSignUp';
+export { default as handleUpdateProfilePhoto } from './authentication/handleUpdateProfilePhoto';
 export { default as mapFirebaseAuthErrors } from './authentication/mapFirebaseAuthErrors';
-export { default as handleCheckLoggedInState } from './authentication/handleCheckLoggedInState';
 
 export { default as extractInitials } from './extractInitials';
 export { default as fixedRatingAverage } from './fixedRatingAverage';
