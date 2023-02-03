3a37d89a0051b291bc1d88515594d3bb37d405fc
Jamie McNeil <jamievmcneil@gmail.com>
2023-02-03 10:12:35 -0500

    Add nums to regex test

--- src/utils/validateTextInput.ts
+++ src/utils/validateTextInput.ts
@@ -1,6 +1,6 @@
 /* Validates text input length and against regex, returns true or false */
 const validateTextInput = (text: string) => {
-  const specialCharsRegex = /[^\w\s]/;
+  const specialCharsRegex = /[-'`~!@#$%^&*()_|+=?;:'",.<>{}[\]\\/0-9]/gi;
 
   if (text.trim().length && !specialCharsRegex.test(text)) {
     return true;
