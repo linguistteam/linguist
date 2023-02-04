17a9aec23a0abe2600a873a587e52980fa368ee4
Jamie McNeil <jamievmcneil@gmail.com>
2023-02-04 14:08:28 -0500

    Check truthy text.trim().length val

--- src/utils/validateTextInput.ts
+++ src/utils/validateTextInput.ts
@@ -3,7 +3,7 @@ const validateTextInput = (text: string) => {
   // NOTE: Removes special chars and numbers
   const invalidNameRegex = /[-'`~!@#$%^&*()_|+=?;:'",.<>{}[\]\\/0-9]/gi;
 
-  if (text.trim().length && !invalidNameRegex.test(text)) {
+  if (!!text.trim().length && !invalidNameRegex.test(text)) {
     return true;
   }
 
