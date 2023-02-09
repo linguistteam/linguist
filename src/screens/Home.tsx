c0191611496875fb0b37f56ec3abec23a612b7f5
Jamie McNeil <jamievmcneil@gmail.com>
2023-02-09 09:42:58 -0500

    Grabbing img from device working!

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
-import { Button, Heading, Input, Stack } from 'native-base';
+import { Alert, Button, Heading, Input, Stack } from 'native-base';
 import { collection, doc, getDoc } from 'firebase/firestore';
+import { updateProfile } from 'firebase/auth';
+import * as DocumentPicker from 'expo-document-picker';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
@@ -8,9 +10,10 @@ import { StackNavigatorList } from './StackNavigator';
 import { useUserStore } from '@stores/user';
 import { useAuthErrorStore } from '@stores/errors/authError';
 import { useLoadingStore } from '@stores/loading';
-import { db } from '../../firebaseConfig';
+import { auth, db } from '../../firebaseConfig';
 import { handleLogout, handleUpdateDisplayName, handleUpdateProfilePhoto } from '@utils';
 import { ProfileImage } from '@components/userprofiles';
+import { PermissionsAndroid } from 'react-native';
 
 const Home = () => {
   const navigation = useNavigation<NativeStackNavigationProp<StackNavigatorList, 'HOME'>>();
@@ -31,9 +34,84 @@ const Home = () => {
   //   }
   // });
 
+  // TODO: Move to own file
+  const [photo, setPhoto] = useState(null);
+
+  const checkPermissions = async () => {
+    try {
+      const result = await PermissionsAndroid.check(
+        PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+      );
+
+      if (!result) {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+          {
+            title: 'You need to give storage permission to download and save the file',
+            message: 'App needs access to your camera ',
+            buttonNeutral: 'Ask Me Later',
+            buttonNegative: 'Cancel',
+            buttonPositive: 'OK',
+          },
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+          console.log('You can use the camera');
+          return true;
+        } else {
+          Alert.alert('Error', I18n.t('PERMISSION_ACCESS_FILE'));
+
+          console.log('Camera permission denied');
+          return false;
+        }
+      } else {
+        return true;
+      }
+    } catch (err) {
+      console.warn(err);
+      return false;
+    }
+  };
+
+  async function selectFile() {
+    try {
+      const result = await checkPermissions();
+
+      if (result) {
+        const result = await DocumentPicker.getDocumentAsync({
+          copyToCacheDirectory: false,
+          type: 'image/*',
+        });
+
+        if (result.type === 'success') {
+          // Printing the log realted to the file
+          console.log('res : ' + JSON.stringify(result));
+          // Setting the state to show single file attributes
+          setPhoto(result);
+        }
+      }
+    } catch (err) {
+      setPhoto(null);
+      console.warn(err);
+      return false;
+    }
+  }
+
   // NOTE: For profile updates
   const [displayName, setDisplayName] = useState(user.displayName ?? '');
   const [base64PhotoURL, setBase64PhotoURL] = useState('');
+
+  const handleUpdatePhotoUrl = (photo: string) => {
+    updateProfile(auth.currentUser, {
+      photoURL: photo.uri,
+    })
+      .then(() => console.log('upload success!'))
+      .catch((error: any) => {
+        console.error('Error occurred:', error);
+      });
+  };
+
+  console.log('photo', photo);
+  console.log('user photo', user.photoURL);
 
   return (
     <SafeAreaView>
@@ -47,6 +125,7 @@ const Home = () => {
         Email: {user.email}
       </Heading>
       <ProfileImage name={user.displayName} profileImage={user.photoURL} />
+      {/* <ProfileImage name={user.displayName} profileImage={photo?.uri} /> */}
 
       <Stack space={4} w="75%" maxW="300px" mx="auto" alignItems="center">
         <Input
@@ -74,6 +153,9 @@ const Home = () => {
         >
           Update profile photo
         </Button>
+
+        <Button onPress={selectFile}>Select photo</Button>
+        <Button onPress={handleUpdatePhotoUrl}>Upload photo</Button>
       </Stack>
 
       <Heading size="sm" textAlign="center" mt={10}>
