24bce14493b119c77eaa0268fa2f016f85931d4b
Jamie McNeil <jamievmcneil@gmail.com>
2023-02-09 14:53:51 -0500

    handleUploadImageToStorage should use uploaded image to update user photo url

--- src/utils/storage/handleUploadImageToStorage.ts
+++ src/utils/storage/handleUploadImageToStorage.ts
@@ -1,6 +1,7 @@
+import { updateProfile } from 'firebase/auth';
 import { StorageError, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { Loading } from '@stores/loading';
-import { storage } from '../../../firebaseConfig';
+import { auth, storage } from '../../../firebaseConfig';
 
 /* Handle uploading image to Firebase Cloud Storage */
 /* Learn more about uploading files to Cloud Storage: https://firebase.google.com/docs/storage/web/upload-files */
@@ -23,22 +24,23 @@ const handleUploadImageToStorage = async ({
     const response = await fetch(photo);
     const blob = await response.blob();
     const fileName = photo.substring(photo.lastIndexOf('/') + 1);
-    console.log('fileName', fileName);
     const storageRef = ref(storage, fileName);
 
     uploadBytes(storageRef, blob)
       .then((snapshot) => {
-        console.log('Uploaded file!', snapshot);
-        getDownloadURL(snapshot.ref)
-          .then((downloadURL) => {
-            console.log('url', downloadURL);
-            setPhoto(null);
-            setLoading({ isLoading: false });
-          })
-          .catch((error: StorageError) => {
-            // TODO: Map all storage errors and handle setting/mapping errors here
-            console.log('error', error);
-          });
+        return snapshot.ref;
+      })
+      .then((snapshotRef) => {
+        return getDownloadURL(snapshotRef);
+      })
+      .then((photoURL) => {
+        console.log('photoURL', photoURL);
+        return updateProfile(auth.currentUser, {
+          photoURL,
+        });
+      })
+      .then(() => {
+        setLoading({ isLoading: false });
       })
       .catch((error: StorageError) => {
         // TODO: Map all storage errors and handle setting/mapping errors here
