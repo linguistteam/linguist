cc325dea41e207b024b95439ab066ebab8593976
Jamie McNeil <jamievmcneil@gmail.com>
2023-02-22 08:11:14 -0500

    Add HandleReauthenticateUserProps; Send in password prop

--- src/utils/authentication/handleReauthenticateUser.ts
+++ src/utils/authentication/handleReauthenticateUser.ts
@@ -1,15 +1,18 @@
 import { auth } from '../../../firebaseConfig';
 import { AuthError, EmailAuthProvider, reauthenticateWithCredential } from 'firebase/auth';
 
-// TODO: Pass user enter password as prop
-const handleReauthenticateUser = () => {
+interface HandleReauthenticateUserProps {
+  password: string;
+}
+
+const handleReauthenticateUser = ({ password }: HandleReauthenticateUserProps) => {
   if (auth.currentUser && auth.currentUser.email) {
-    const password = 'Grab from user input';
     const credentials = EmailAuthProvider.credential(auth.currentUser.email, password);
 
     reauthenticateWithCredential(auth.currentUser, credentials)
       .then(() => {
         // User reauthenticated
+        console.log('User reauthenticated!');
       })
       .catch((error: AuthError) => {
         console.error('The following error has occurred: ', error.code);
