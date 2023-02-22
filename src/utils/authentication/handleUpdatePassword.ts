10b028cfd4dde9f56ed24b209ec02a965046876f
Jamie McNeil <jamievmcneil@gmail.com>
2023-02-22 09:17:23 -0500

    Add loading/error states

--- src/utils/authentication/handleUpdatePassword.ts
+++ src/utils/authentication/handleUpdatePassword.ts
@@ -1,20 +1,49 @@
 import { auth } from '../../../firebaseConfig';
-import { AuthError, EmailAuthProvider, reauthenticateWithCredential } from 'firebase/auth';
+import {
+  AuthError,
+  EmailAuthProvider,
+  reauthenticateWithCredential,
+  updatePassword,
+} from 'firebase/auth';
+import { FirebaseError } from '@stores/errors/firebaseError';
+import { Loading } from '@stores/loading';
+import mapFirebaseAuthErrors from './mapFirebaseAuthErrors';
+
+/* Handle updating user password */
+/* Learn more about updating user password: https://firebase.google.com/docs/auth/web/manage-users#set_a_users_password */
 
 interface HandleUpdatePasswordProps {
-  password: string;
+  currentPassword: string;
+  newPassword: string;
+  setError: ({ errorMessage, errorCode }: FirebaseError) => void;
+  setLoading: ({ isLoading }: Loading) => void;
 }
 
-const handleUpdatePassword = ({ password }: HandleUpdatePasswordProps) => {
-  if (auth.currentUser && auth.currentUser.email) {
-    const credentials = EmailAuthProvider.credential(auth.currentUser.email, password);
+const handleUpdatePassword = ({
+  currentPassword,
+  newPassword,
+  setError,
+  setLoading,
+}: HandleUpdatePasswordProps) => {
+  const user = auth.currentUser;
 
-    reauthenticateWithCredential(auth.currentUser, credentials)
+  if (user && user.email) {
+    setLoading({ isLoading: true });
+
+    const credentials = EmailAuthProvider.credential(user.email, currentPassword);
+
+    reauthenticateWithCredential(user, credentials)
       .then(() => {
-        console.log('User reauthenticated!');
+        // User reauthenticated
+        return updatePassword(user, newPassword);
+      })
+      .then(() => {
+        setLoading({ isLoading: false });
       })
       .catch((error: AuthError) => {
         console.error('The following error has occurred: ', error.code);
+        setError({ errorMessage: mapFirebaseAuthErrors(error.code), errorCode: error.code });
+        setLoading({ isLoading: false });
       });
   }
 };
